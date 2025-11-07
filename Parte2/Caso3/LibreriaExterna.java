72d8f977a2a7b52ab62319093f6b1cead10604f2
JPpittigotti <jppittigotti@gmail.com>
2025-11-06 22:09:25 -0500

    Finalización del caso 3

--- Parte2/Caso3/LibreriaExterna.java
+++ Parte2/Caso3/LibreriaExterna.java
@@ -9,16 +9,34 @@ public class LibreriaExterna {
     }
 }
 
+//Se crea esta interfaz para definir el motodo que espera el sistema
+interface InterfazAdaptada {
+
+    void funcionAdaptada();
+    
+}
+
 // Adaptador base - a modificar para patron adapter
-class Adaptador{
+class Adaptador implements InterfazAdaptada{ // Implementamos la interfaz
     private LibreriaExterna externa;
 
     public Adaptador(){
         externa = new LibreriaExterna();
     }
 
+    //Metodo adaptado
+    @Override
     public void funcionAdaptada(){
+        //llamamos al metodo del la libreria externa
         externa.metodoExterno();
     }
 
+}
+
+//Main de prueba
+class Main {
+    public static void main(String[] args) {
+        InterfazAdaptada objeto = new Adaptador(); // se usa la interfaz creada
+        objeto.funcionAdaptada(); // llamada al metodo adaptado
+    }
 }
